1cd718cb9ddc042b5528b0147afef381a86628e3
Pinchez25 <thuamwangi001@gmail.com>
2025-03-06 13:56:04 +0300

    Implement comprehensive savings application form submission and initialisation

--- app/src/savings-application/savings-application.component.js
+++ app/src/savings-application/savings-application.component.js
@@ -22,7 +22,43 @@
         vm.getSavingsTemplate = getSavingsTemplate;
         vm.clearForm = clearForm;
         vm.submit = submit;
-        vm.form = {};
+
+        init();
+
+        function init() {
+            AccountService.getClientId().then(function (clientId) {
+                vm.clientId = clientId;
+                getSavingsTemplate(clientId, null);
+            });
+        }
+
+        function getSavingsTemplate(clientId, productId) {
+            SavingsApplicationService.template().get({
+                clientId: clientId,
+                productId: productId
+            }).$promise.then(function(template) {
+                if (!productId) {
+                    vm.template = template;
+                } else {
+                    vm.form = {
+                        ...vm.form,
+                        productId: template.savingsProductId,
+                        nominalAnnualInterest: template.nominalAnnualInterestRate,
+                        interestCompoundingPeriodType: template.interestCompoundingPeriodType.id,
+                        interestPostingPeriodType: template.interestPostingPeriodType.id,
+                        interestCalculationType: template.interestCalculationType.id,
+                        interestCalculationDaysInYearType: template.interestCalculationDaysInYearType.id,
+                        submittedOnDate: $filter('date')(new Date(), 'dd MMMM yyyy')
+                    };
+                    
+                    vm.template.interestCompoundingPeriodTypeOptions = template.interestCompoundingPeriodTypeOptions;
+                    vm.template.interestPostingPeriodTypeOptions = template.interestPostingPeriodTypeOptions;
+                    vm.template.interestCalculationTypeOptions = template.interestCalculationTypeOptions;
+                    vm.template.interestCalculationDaysInYearTypeOptions = template.interestCalculationDaysInYearTypeOptions;
+                    vm.template.currency = template.currency;
+                }
+            });
+        }
 
         function clearForm() {
             $scope.savingsApplicationForm.$setPristine();
