9b5ca714424ed938876110c6f5d84d51f03785a8
Pinchez25 <thuamwangi001@gmail.com>
2025-03-06 16:38:32 +0300

    Implement shares application form submission and account loading functionality

--- app/src/shares-application/shares-application.component.js
+++ app/src/shares-application/shares-application.component.js
@@ -23,7 +23,73 @@
         vm.getSharesTemplate = getSharesTemplate;
         vm.clearForm = clearForm;
         vm.submit = submit;
-        vm.form = {};
+        vm.loadSavingsAccounts = loadSavingsAccounts;
+
+        init();
+
+        function init() {
+            AccountService.getClientId().then(function (clientId) {
+                vm.clientId = clientId;
+                getSharesTemplate(clientId, null);
+                loadSavingsAccounts(clientId);
+            });
+        }
+
+        function loadSavingsAccounts(clientId) {
+            AccountService.getAllAccounts(clientId).get().$promise.then(function(accounts) {
+                console.log('Savings accounts:', accounts);
+                vm.savingsAccounts = accounts.savingsAccounts || [];
+            }, function(error) {
+                console.error('Error fetching savings accounts:', error);
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content("Error loading savings accounts")
+                        .hideDelay(2000)
+                        .position('top right')
+                );
+            });
+        }
+
+        function getSharesTemplate(clientId, productId) {
+            var existingProductOptions = vm.template.productOptions;
+            console.log('Fetching shares template with:', { clientId: clientId, productId: productId });
+            
+            SharesApplicationService.template().get({
+                clientId: clientId,
+                productId: productId
+            }).$promise.then(function(template) {
+                console.log('Shares template response:', template);
+                if (!productId) {
+                    vm.template = template;
+                } else {
+                    vm.form = Object.assign({}, vm.form, {
+                        productId: template.productId || productId,
+                        requestedShares: null,
+                        savingsAccountId: null,
+                        submittedDate: $filter('date')(new Date(), 'dd MMMM yyyy'),
+                        applicationDate: new Date()
+                    });
+                    
+                    vm.template = Object.assign({}, vm.template, {
+                        currency: template.currency,
+                        savingsAccountOptions: template.savingsAccountOptions,
+                        minimumShares: template.minimumShares,
+                        nominalPrice: template.unitPrice,
+                        productOptions: existingProductOptions
+                    });
+                }
+                console.log('Updated template:', vm.template);
+                console.log('Updated form:', vm.form);
+            }, function(error) {
+                console.error('Error fetching shares template:', error);
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content("Error loading share products")
+                        .hideDelay(2000)
+                        .position('top right')
+                );
+            });
+        }
 
         function clearForm() {
             $scope.shareApplicationForm.$setPristine();
