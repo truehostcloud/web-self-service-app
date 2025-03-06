2f971e235b75c8e081fbefe83ac5e0df8f933f60
Pinchez25 <thuamwangi001@gmail.com>
2025-03-06 16:39:39 +0300

    Add resource methods for shares application service

--- app/src/shares-application/shares-application.service.js
+++ app/src/shares-application/shares-application.service.js
@@ -9,9 +9,25 @@
      * @description
      * Service required for Shares Application
      */
-    function SharesApplicationService() {
+    function SharesApplicationService($resource, BASE_URL) {
+        this.submitApplication = function() {
+            return $resource(BASE_URL + '/self/shareaccounts', {}, {
+                submit: {
+                    method: 'POST'
+                }
+            });
+        }
 
-
+        this.template = function() {
+            return $resource(BASE_URL + '/self/shareaccounts/template', {}, {
+                get: {
+                    method: 'GET',
+                    params: {
+                        clientId: '@clientId'
+                    }
+                }
+            });
+        }
     }
 
 })();
