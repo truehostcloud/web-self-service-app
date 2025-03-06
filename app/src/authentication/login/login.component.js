c8d3c8bf3c1a0fef3c026f0d12abfac1e1147c3d
Pinchez25 <thuamwangi001@gmail.com>
2025-03-06 10:07:18 +0300

    Remove debug logging from login component

--- app/src/authentication/login/login.component.js
+++ app/src/authentication/login/login.component.js
@@ -14,19 +14,15 @@
          * @description To perform the login action on the page
          */
         $scope.doLogin = function () {
-            console.log('Login attempt with data:', $scope.loginData);
             vm.authenticating = true;
             AuthService.doLogin().save($scope.loginData).$promise
                 .then(function (result) {
-                    console.log('Login response:', result);
                     AuthService.setUser(result);
                     AccountService.getClients().get().$promise
                         .then(function (res) {
-                            console.log('Client data:', res);
                             vm.authenticating = false;
                             $state.go("app.dashboard");
                             if (res.pageItems.length !== 0) {
-                                console.log('Selected client ID:', res.pageItems[0].id);
                                 AccountService.setClientId(res.pageItems[0].id);
                                 $mdToast.show(
                                     $mdToast.simple()
@@ -36,7 +32,6 @@
                                 );
                 
                             } else {
-                                console.log('No clients found for user');
                                 $mdToast.show(
                                     $mdToast.simple()
                                         .content("No Clients Found")
@@ -47,7 +42,6 @@
                             }
                         })
                         .catch(function (error) {
-                            console.error('Client fetch error:', error);
                             vm.authenticating = false;
                             $mdToast.show(
                                 $mdToast.simple()
@@ -58,7 +52,6 @@
                             AuthService.logout();
                         })
                 }).catch(function (error) {
-                    console.error('Login error:', error);
                     vm.authenticating = false;
                     $mdToast.show(
                         $mdToast.simple()
