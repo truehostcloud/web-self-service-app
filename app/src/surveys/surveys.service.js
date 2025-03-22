8c56a429df4cbebd937bc2f8f14121d31e88dbb6
Pinchez25 <thuamwangi001@gmail.com>
2025-03-22 11:02:29 +0300

    Add SurveysService to handle survey data retrieval and submission

---
+++ app/src/surveys/surveys.service.js
@@ -0,0 +1,30 @@
+(function(){
+    'use strict';
+    
+    angular.module('selfService')
+        .service('SurveysService', ['$http', '$resource', 'BASE_URL', SurveysService]);
+
+    function SurveysService($http, $resource, BASE_URL) {
+        this.getAllSurveys = function() {
+            return $http({
+                method: 'GET',
+                url: BASE_URL + '/self/surveys'
+            });
+        };
+        
+        this.getClientSurveys = function(clientId) {
+            return $http({
+                method: 'GET',
+                url: BASE_URL + '/surveys/scorecards/clients/' + clientId
+            });
+        };
+        
+        this.submitSurvey = function(surveyId, formData) {
+            return $http({
+                method: 'POST',
+                url: BASE_URL + '/surveys/scorecards/' + surveyId,
+                data: formData
+            });
+        };
+    }
+})(); 
