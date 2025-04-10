2b25ef7b6392980a5fd7a73d09cd2b29c4bedcc2
Pinchez25 <thuamwangi001@gmail.com>
2025-04-10 13:00:12 +0300

    Add methods to retrieve and update client survey submissions in surveys.service.js. Introduced getLatestClientSurveySubmissions and getClientSurveySubmission for fetching data, and updateSurvey for modifying existing submissions with formatted timestamps.

--- app/src/surveys/surveys.service.js
+++ app/src/surveys/surveys.service.js
@@ -19,11 +19,50 @@
             });
         };
 
+        this.getLatestClientSurveySubmissions = function(clientId) {
+            return $http({
+                method: 'GET',
+                url: BASE_URL + '/self/surveys/scorecards/clients/' + clientId + '/latest'
+            });
+        };
+
+        this.getClientSurveySubmission = function(surveyId, clientId) {
+            return $http({
+                method: 'GET',
+                url: BASE_URL + '/self/surveys/scorecards/' + surveyId + '/clients/' + clientId
+            });
+        };
+
         this.submitSurvey = function(surveyId, formData) {
             return $http({
                 method: 'POST',
                 url: BASE_URL + '/self/surveys/scorecards/' + surveyId,
                 data: formData
+            });
+        };
+
+        this.updateSurvey = function(surveyId, clientId, formData) {
+            var now = new Date();
+            // For ScorecardData.createdOn (LocalDateTime)
+            var localDateTimeFormat = now.toISOString().split('.')[0].replace('Z', '');
+            // For ScorecardValue.createdOn (OffsetDateTime)
+            var offsetDateTimeFormat = now.toISOString().split('.')[0] + 'Z';
+
+            return $http({
+                method: 'PUT',
+                url: BASE_URL + '/self/surveys/scorecards/' + surveyId + '/clients/' + clientId,
+                data: {
+                    clientId: parseInt(clientId),
+                    createdOn: localDateTimeFormat,
+                    scorecardValues: formData.scorecardValues.map(function(response) {
+                        return {
+                            questionId: parseInt(response.questionId),
+                            responseId: parseInt(response.responseId),
+                            value: parseInt(response.value),
+                            createdOn: offsetDateTimeFormat
+                        };
+                    })
+                }
             });
         };
     }
